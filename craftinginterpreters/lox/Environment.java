dda46b91ac26e18f0ab55228d12b64c370b43dc4
Stella Zarei <zareistella@gmail.com>
2024-10-02 14:11:42 -0700

    Update environment to support scopes

--- craftinginterpreters/lox/Environment.java
+++ craftinginterpreters/lox/Environment.java
@@ -9,15 +9,30 @@ import java.util.Map;
  * 
  */
 public class Environment {
+    // Reference to the enclosing scope environment 
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     /**
      * Look up variable by name
      * @throws RuntimeError if the variable is not found
      */
     Object get(Token name) {
+        // Look in the current scope
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
+        }
+        // Look in the enclosing scope
+        if (enclosing != null) {
+            return enclosing.get(name);
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
@@ -28,6 +43,25 @@ public class Environment {
      */
     void define(String name, Object value) {
         values.put(name, value);
+    }
+
+    /**
+     * Modifies the value bound to an existing variable
+     * Lox does not support implicit variable declaration 
+     * 
+     * @throws RuntimeError if the variable is not found
+     */
+    void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme)) {
+            values.put(name.lexeme, value);
+            return;
+        }
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
+        throw new RuntimeError(name, "Undefined varibale '" + name.lexeme + "'.");
     }
 
 
