d805892b18abf4cb61b19ac10a0fb9daf7cbf255
Stella Zarei <zareistella@gmail.com>
2024-10-03 15:45:40 -0700

    Adds parsing 'for' loops using exisiting functionality

--- craftinginterpreters/lox/Parser.java
+++ craftinginterpreters/lox/Parser.java
@@ -3,6 +3,7 @@ package craftinginterpreters.lox;
 import static craftinginterpreters.lox.TokenType.*;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 
 /**
@@ -68,6 +69,7 @@ public class Parser {
      * statement -> exprStmt | printStmt | block | ifStmt | whileStmt | forStmt
      */
     private Stmt statement() {
+        if (match(FOR)) return forStatement();
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(WHILE)) return whileStatement();
@@ -137,12 +139,58 @@ public class Parser {
     }
 
     /**
+     * forStmt -> "for" "(" ( varDecl | exprStmt | ";" ) expression? ";" expression? ")" statement
+     */
+    private Stmt forStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+        Stmt initializer;
+        // for (;... | for (var x = ... | for (x = ...
+        if (match(SEMICOLON)) {
+            initializer = null;
+        } else if (match(VAR)) {
+            initializer = varDeclaration();
+        } else {
+            initializer = expressionStatement();
+        }
+
+        Expr condition = null;
+        if (!check(SEMICOLON)) {
+            condition = expression();
+        }
+        consume(SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = null;
+        if (!check(RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(
+                body,
+                new Stmt.Expression(increment)
+            ));
+        }
+
+        if (condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(initializer, body));
+        }
+
+        return body;
+    }
+
+    /**
      * expression -> assignment
      */
     private Expr expression() {
         return equality();
     }
-
 
     /**
      * assignment -> IDENTIFIER "=" assignment | logic_or
